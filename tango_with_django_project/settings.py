fadfa6b6342034dbe1b3a6621d38c312ee0c95a3
Fatima kamran <2917308K@student.gla.ac.uk>
2025-02-08 13:42:26 +0000

    implemented user registration

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -81,6 +81,13 @@ TEMPLATES = [
     },
 ]
 
+PASSWORD_HASHERS = [
+'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+'django.contrib.auth.hashers.BCryptPasswordHasher',
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
