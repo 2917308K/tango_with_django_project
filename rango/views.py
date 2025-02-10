f59aea38f343881c22a7bf0ad99655093a21a95a
Fatima kamran <2917308K@student.gla.ac.uk>
2025-02-10 00:44:16 +0000

    implemented cookie handling + chapter 10 exercises

--- rango/views.py
+++ rango/views.py
@@ -9,9 +9,12 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
+    visitor_cookie_handler(request)
+
     category_list = Category.objects.order_by('-likes')[:5]
     pages = Page.objects.order_by('-views')[:5]
     
@@ -20,12 +23,19 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = pages
     
-    return render(request, 'rango/index.html', context=context_dict)
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
+    visitor_cookie_handler(request)
+
     print(request.method)
     print(request.user)
-    return render(request, 'rango/about.html', {})
+
+    context_dict = {}
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -151,5 +161,25 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
 
+def visitor_cookie_handler(request):
+    visits = int(request.session.get('visits', '1'))
 
+    last_visit_cookie = request.session.get('last_visit', str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+  
+    
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 #Redirects users who are not logged in
 LOGIN_URL = 'rango:login'
 
